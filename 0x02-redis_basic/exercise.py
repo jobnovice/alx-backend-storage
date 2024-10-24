82a9efd02e0d2b6649ba9812b2581280076fae0e
jobnovice <jobkebede47@gmail.com>
2024-10-24 22:27:07 +0300

    ooh

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""installed and configured redis to writing strings to it """
+from typing import Union
+import redis
+import uuid
+
+
+class Cache():
+    """created a class for storing an instance of redis as a client"""
+    def __init__(self):
+        self._redis = redis.Redis()
+        self. _redis.flushdb()
+
+    def store(self, data: Unions[int, float, bytes, str]) -> str:
+        """stores the inputed data and returns the random key"""
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+
+        return random_key
